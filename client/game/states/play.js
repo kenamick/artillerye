64d1cede9c113ca463b911e11819d496e0efe847
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-06-24 00:01:51 +0200

    Attempt to add fine collision

--- client/game/states/play.js
+++ client/game/states/play.js
@@ -14,10 +14,20 @@
       this.game.physics.p2.gravity.y = 300;
 
       // add terrain
-      var terrain = this.game.add.sprite(gw/2, gh - 50 + 50/2, this.createTerrain());
+      var polyTerrain = this.createTerrainPoly(gw, 70);
+      var terrain = this.game.add.sprite(gw/2, gh - 70 + 70/2,
+        this.createTerrain(gw, 70, polyTerrain));
 
       // create physics body for this sprite
       this.game.physics.p2.enable(terrain, true);
+
+      //TODO: use http://schteppe.github.io/p2.js/demos/heightfield.html
+      terrain.body.clearShapes();
+      terrain.body.addPolygon({
+        'skipSimpleCheck': true,
+        'removeCollinearPoints': true
+      }, polyTerrain);
+
       // terrain.anchor.setTo(0, 0);
       terrain.body.static = true;
       // terrain.body.collideWorldBounds = true;
@@ -44,54 +54,57 @@
       this.game.state.start('gameover');
     },
 
-    createTerrain: function() {
+    createTerrainPoly: function(width, height) {
+      var poly = []
+        , amp = 1
+        , amps = 0.2
+        , baseY = height / 2;
 
-      var gw = this.game.width;
-      var gh = this.game.height;
+      for(var i = 0; i < width; i+=10) {
+        var pt = [
+          i,
+          baseY + amp * Math.sin(i * Math.PI / 180.0)
+        ];
+        poly.push(pt);
 
-      var bmd = this.game.add.bitmapData(gw, 50);
+        if (amp >= height / 2)
+          amps = -amps;
 
+        amp += amps;
+      }
+
+      poly.push([pt[0], height]);
+      poly.push([0, height]);
+      poly.push([0, baseY]);
+
+      return poly;
+    },
+
+    createTerrain: function(width, height, poly) {
+      var bmd = this.game.add.bitmapData(width, height);
       bmd.ctx.beginPath();
-      bmd.ctx.rect(0, 0, gw, 50);
+
+      // bmd.ctx.beginPath();
+      // bmd.ctx.rect(0, 0, width, height);
+      // bmd.ctx.fillStyle = '#ff0000';
+      // bmd.ctx.fill();
+      // return bmd;
+
+      bmd.ctx.moveTo(poly[0][0], poly[0][1]);
+
+      for(var i = 0, count = poly.length; i < count; i++) {
+        var p = poly[i];
+        bmd.ctx.lineTo(p[0], p[1]);
+        console.log('line to ', p);
+      }
+      // bmd.ctx.lineTo(p[0], height);
+      // bmd.ctx.lineTo(0, height);
+      // bmd.ctx.lineTo(0, 0);
+
       bmd.ctx.fillStyle = '#ff0000';
       bmd.ctx.fill();
+
       return bmd;
-
-      var terrainPts = [];
-      var amp = 1;
-      var baseY = 270;
-
-      for(var i = 0; i < gw; i++) {
-        amp += 0.5;
-        var pt = {
-          x: i,
-          y: baseY
-          //amp * Math.sin(i * Math.PI / 180.0)
-        };
-        terrainPts.push(pt);
-      }
-
-      var graphics = this.game.add.graphics(0, 0);
-
-      // set a fill and line style
-      graphics.beginFill(0xFF3300);
-      graphics.lineStyle(2, 0xffd900, 1);
-      // graphics.strokeStyle('rgb(0,255,255)');
-
-      graphics.moveTo(0, baseY);
-
-      for(var i = 0; i < gw; i++) {
-        var p = terrainPts[i];
-        // draw a shape
-        // console.log('line to ', p.x, p.y);
-        graphics.lineTo(p.x, p.y);
-      }
-      graphics.lineTo(gw, gh);
-      graphics.lineTo(0, gh);
-      graphics.lineTo(0, baseY);
-      graphics.endFill();
-
-      return graphics;
     }
 
   };
