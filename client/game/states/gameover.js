03234831a50c54e3dbbf8e183cf76e93edc6d0c6
Petar Petrov <petar.petrov.georgiev@gmail.com>
2015-01-12 01:35:01 +0100

    Fix background on game over screen

--- client/game/states/gameover.js
+++ client/game/states/gameover.js
@@ -17,11 +17,14 @@ GameOver.prototype = {
   },
   create: function () {
     var style = { font: '65px Arial', fill: '#ffffff', align: 'center'};
+
+    this.game.stage.backgroundColor = '#000000';
+
     this.titleText = this.game.add.text(this.game.world.centerX,100, 'Game Over!', style);
     this.titleText.anchor.setTo(0.5, 0.5);
 
-    this.congratsText = this.game.add.text(this.game.world.centerX, 200, 'You Win!', { font: '32px Arial', fill: '#ffffff', align: 'center'});
-    this.congratsText.anchor.setTo(0.5, 0.5);
+    // this.congratsText = this.game.add.text(this.game.world.centerX, 200, 'You Win!', { font: '32px Arial', fill: '#ffffff', align: 'center'});
+    // this.congratsText.anchor.setTo(0.5, 0.5);
 
     this.instructionText = this.game.add.text(this.game.world.centerX, 300, 'Click To Play Again', { font: '16px Arial', fill: '#ffffff', align: 'center'});
     this.instructionText.anchor.setTo(0.5, 0.5);
